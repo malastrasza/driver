0e0ef744fd940164f5e5cfd009ba2dd0dbb17b75
malastrasza <malastrasza@gmail.com>
2019-01-20 18:27:28 +0100

    mobile version started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <div className="headerBlack"></div>
+                <div className="headerChoice"></div>
+            </div>
+        );
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (<div className="mainComponent">
+            <Header/>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-      hello page
-      </div>
-    );
-  }
+    render() {
+        return <Main/>
+    }
 }
 
 export default App;
