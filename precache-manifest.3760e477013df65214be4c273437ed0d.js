1130aa20bbbe88b922627e454a442a66a16afd8b
malastrasza <malastrasza@gmail.com>
2019-02-09 20:51:27 +0100

    Updates

---
+++ precache-manifest.3760e477013df65214be4c273437ed0d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c7ccc34121c5e3e4b74d1a5e52925e",
+    "url": "/driver/static/media/drivingexperience.e3c7ccc3.jpg"
+  },
+  {
+    "revision": "4ff8c77a215886c69ad8f139494810ed",
+    "url": "/driver/static/media/crash.4ff8c77a.mp4"
+  },
+  {
+    "revision": "617f0b85274bb22f4f02c68f5797db9c",
+    "url": "/driver/static/media/carcrash.617f0b85.jpeg"
+  },
+  {
+    "revision": "26e27f3d1bc6b06fb292",
+    "url": "/driver/static/js/runtime~main.26e27f3d.js"
+  },
+  {
+    "revision": "74a6fe6ebe694ba9d71e",
+    "url": "/driver/static/js/main.74a6fe6e.chunk.js"
+  },
+  {
+    "revision": "3d33b19012e506ce8509",
+    "url": "/driver/static/js/1.3d33b190.chunk.js"
+  },
+  {
+    "revision": "74a6fe6ebe694ba9d71e",
+    "url": "/driver/static/css/main.d6369518.chunk.css"
+  },
+  {
+    "revision": "3d33b19012e506ce8509",
+    "url": "/driver/static/css/1.31c647a1.chunk.css"
+  },
+  {
+    "revision": "ede73e3b040489b98b9c1f1138fb32d5",
+    "url": "/driver/index.html"
+  }
+];
